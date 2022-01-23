d26c00cc427135e6f074d8c5b8bda65189c22530
sirasit <sirasit@siruxsolutions.com>
2022-01-23 12:31:36 -0800

    Updates

---
+++ precache-manifest.42b6c33bfd077a8cb8e48fe28652b3c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dba3bf139d49b26c6d696c7cecebf59",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "aac965a2db5372c59910",
+    "url": "/keeper/static/js/2.246fee9e.chunk.js"
+  },
+  {
+    "revision": "8ec773caf63b6bd31f59",
+    "url": "/keeper/static/js/main.699a55eb.chunk.js"
+  },
+  {
+    "revision": "904356601dc1001bd3a5",
+    "url": "/keeper/static/js/runtime-main.7142975c.js"
+  }
+]);
